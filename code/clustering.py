f36d1e30a3abc759fee3981fd16fbd546d619de2
freddiejones <fredjjones1700@gmail.com>
2023-12-06 12:13:32 -0500

    merged seasonal dataframes into one large one

--- code/clustering.py
+++ code/clustering.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import requests
 from bs4 import BeautifulSoup
+from pyts.approximation import SymbolicAggregateApproximation
 
 def scrape_current_season(url):
     # URL to current szn stats
@@ -179,6 +180,7 @@ def clean_dataframes(dict):
 
 def main():
 
+    # URLs for current season data
     per_game_url = "https://www.basketball-reference.com/leagues/NBA_2024.html#per_game-team"
     advanced_url = "https://www.basketball-reference.com/leagues/NBA_2024.html#advanced-team"
 
@@ -186,107 +188,117 @@ def main():
     per_game_data = scrape_current_season(per_game_url)
     advanced_data = scrape_advanced_team_stats(advanced_url)
 
-    print(per_game_data.head())
-    print(advanced_data.head())
 
-    # # Paths to CSV data
-    # folder_path = "/Users/freddiejones/Desktop/CIS563/project/szn-stats"
-    # adv_folder_path = "/Users/freddiejones/Desktop/CIS563/project/advanced-stats"
+    # Paths to CSV data
+    folder_path = "/Users/freddiejones/Desktop/CIS563/project/szn-stats"
+    adv_folder_path = "/Users/freddiejones/Desktop/CIS563/project/advanced-stats"
 
-    # # Read in CSVs, clean data
-    # all_seasonal_dataframes = read_season_stats(folder_path)
-    # clean_seasonal_dataframes = clean_dataframes(all_seasonal_dataframes)
+    # Read in CSVs, clean data
+    all_seasonal_dataframes = read_season_stats(folder_path)
+    clean_seasonal_dataframes = clean_dataframes(all_seasonal_dataframes)
     
-    # all_advanced_dataframes = read_advanced_stats(adv_folder_path)
-    # clean_advanced_dataframes = clean_dataframes(all_advanced_dataframes)
+    all_advanced_dataframes = read_advanced_stats(adv_folder_path)
+    clean_advanced_dataframes = clean_dataframes(all_advanced_dataframes)
 
-    # merged_data = merge_dataframes(clean_seasonal_dataframes, clean_advanced_dataframes)
-    # clean_merged_data = clean_dataframes(merged_data)
-    # print(clean_merged_data)
-
-    # # Mapping teams to logos
-    # logo_folder = '/Users/freddiejones/Desktop/CIS563/project/logos/'
-    # team_image_mapping = {
-    #     'boston celtics': 'celtics.png',
-    #     'los angeles clippers': 'clippers.png',
-    #     'houston rockets': 'rockets.png',
-    #     'minnesota timberwolves': 'timberwolves.png',
-    #     'portland trail blazers': 'blazers.png',
-    #     'oklahoma city thunder': 'thunder.png',
-    #     'san antonio spurs': 'spurs.png',
-    #     'phoenix suns': 'suns.png',
-    #     'dallas mavericks': 'mavericks.png',
-    #     'denver nuggets': 'nuggets.png',
-    #     'golden state warriors': 'warriors.png',
-    #     'los angeles lakers': 'lakers.png',
-    #     'miami heat': 'heat.png',
-    #     'toronto raptors': 'raptors.png',
-    #     'atlanta hawks': 'hawks.png',
-    #     'detroit pistons': 'pistons.png',
-    #     'washington wizards': 'wizards.png',
-    #     'sacramento kings': 'kings.png',
-    #     'new orleans pelicans': 'pelicans.png',
-    #     'philadelphia 76ers': 'sixers.png',
-    #     'new york knicks': 'knicks.png',
-    #     'brooklyn nets': 'nets.png',
-    #     'cleveland cavaliers': 'cavaliers.png',
-    #     'charlotte bobcats': 'hornets.png',
-    #     'indiana pacers': 'pacers.png',
-    #     'orlando magic': 'magic.png',
-    #     'memphis grizzlies': 'grizzlies.png',
-    #     'milwaukee bucks': 'bucks.png',
-    #     'utah jazz': 'jazz.png',
-    #     'chicago bulls': 'bulls.png'
-    # }
+    # Dictionary with dataframe of stats for every season
+    merged_data = merge_dataframes(clean_seasonal_dataframes, clean_advanced_dataframes)
+    clean_merged_data = clean_dataframes(merged_data)
     
-    # # Clustering.....
-    # columns_for_clustering = ['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P',
-    #                           '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL',
-    #                           'BLK', 'TOV', 'PF', 'PTS', 'Age', 'W', 'L', 'PW',
-    #                           'PL', 'MOV', 'SOS', 'SRS', 'ORtg', 'DRtg', 'NRtg', 'Pace', 'FTr',
-    #                           '3PAr', 'TS%', 'eFG%', 'TOV%', 'ORB%', 'FT/FGA', 'eFG%.1', 'TOV%.1',
-    #                           'DRB%', 'FT/FGA.1']
+    # Concatenate all dataframes into a single dataframe for clustering
+    every_szn_df = pd.concat(clean_merged_data.values(), ignore_index=True)
+    every_szn_df['Team'] = every_szn_df['Season_Year'].astype(str) + ' ' + every_szn_df['Team']
+    print(every_szn_df)
+
+    # Mapping teams to logos
+    logo_folder = '/Users/freddiejones/Desktop/CIS563/project/logos/'
+    team_image_mapping = {
+        'boston celtics': 'celtics.png',
+        'los angeles clippers': 'clippers.png',
+        'houston rockets': 'rockets.png',
+        'minnesota timberwolves': 'timberwolves.png',
+        'portland trail blazers': 'blazers.png',
+        'oklahoma city thunder': 'thunder.png',
+        'san antonio spurs': 'spurs.png',
+        'phoenix suns': 'suns.png',
+        'dallas mavericks': 'mavericks.png',
+        'denver nuggets': 'nuggets.png',
+        'golden state warriors': 'warriors.png',
+        'los angeles lakers': 'lakers.png',
+        'miami heat': 'heat.png',
+        'toronto raptors': 'raptors.png',
+        'atlanta hawks': 'hawks.png',
+        'detroit pistons': 'pistons.png',
+        'washington wizards': 'wizards.png',
+        'sacramento kings': 'kings.png',
+        'new orleans pelicans': 'pelicans.png',
+        'philadelphia 76ers': 'sixers.png',
+        'new york knicks': 'knicks.png',
+        'brooklyn nets': 'nets.png',
+        'cleveland cavaliers': 'cavaliers.png',
+        'charlotte bobcats': 'hornets.png',
+        'indiana pacers': 'pacers.png',
+        'orlando magic': 'magic.png',
+        'memphis grizzlies': 'grizzlies.png',
+        'milwaukee bucks': 'bucks.png',
+        'utah jazz': 'jazz.png',
+        'chicago bulls': 'bulls.png'
+    }
+    
+    # Clustering.....
+    columns_for_clustering = ['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P',
+                              '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL',
+                              'BLK', 'TOV', 'PF', 'PTS', 'Age', 'W', 'L', 'PW',
+                              'PL', 'MOV', 'SOS', 'SRS', 'ORtg', 'DRtg', 'NRtg', 'Pace', 'FTr',
+                              '3PAr', 'TS%', 'eFG%', 'TOV%', 'ORB%', 'FT/FGA', 'eFG%.1', 'TOV%.1',
+                              'DRB%', 'FT/FGA.1']
     
     # for key, df in clean_merged_data.items():
-    #     # Drop any remaining NaN values
-    #     df = df.dropna()
+    # Drop any remaining NaN values
+    every_szn_df = every_szn_df.dropna()
 
-    #     # Apply KMeans clustering
-    #     scaler = StandardScaler()
-    #     clustering_data = scaler.fit_transform(df[['ORtg', 'DRtg']])
-    #     kmeans = KMeans(n_clusters=6, random_state=26)
-    #     df['KMeans_Cluster'] = kmeans.fit_predict(clustering_data)
+    # Apply KMeans clustering
+    scaler = StandardScaler()
+    clustering_data = scaler.fit_transform(every_szn_df[['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P',
+                              '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL',
+                              'BLK', 'TOV', 'PF', 'PTS', 'Age', 'W', 'L', 'PW',
+                              'PL', 'MOV', 'SOS', 'SRS', 'ORtg', 'DRtg', 'NRtg', 'Pace', 'FTr',
+                              '3PAr', 'TS%', 'eFG%', 'TOV%', 'ORB%']])
 
-    #     # Apply Agglomerative clustering
-    #     agg_clustering = AgglomerativeClustering(n_clusters=8)
-    #     df['Agg_Cluster'] = agg_clustering.fit_predict(clustering_data)
+    
+    # KMeans
+    kmeans = KMeans(n_clusters=6, random_state=26)
+    every_szn_df['KMeans_Cluster'] = kmeans.fit_predict(clustering_data)
 
-    #     # Print the results
-    #     print(f"Results for {key} DataFrame:")
-    #     print(df[['Team', 'KMeans_Cluster', 'Agg_Cluster']])
+    # Apply Agglomerative clustering
+    agg_clustering = AgglomerativeClustering(n_clusters=8)
+    every_szn_df['Agg_Cluster'] = agg_clustering.fit_predict(clustering_data)
 
-    #     fig, ax = plt.subplots(figsize=(8, 6))
-    #     ax.scatter(df['ORtg'], df['DRtg'], c='black', alpha=0)  # Use a constant color and make the points transparent
+    # Print the results
+    print(f"Results for large DataFrame:")
+    print(every_szn_df[['Team', 'KMeans_Cluster', 'Agg_Cluster']])
 
-    #     # Visualize the clusters
-    #     # plt.scatter(df['ORtg'], df['DRtg'], c=df['KMeans_Cluster'], cmap='viridis', label='KMeans Clusters')
-    #     plt.scatter(df['ORtg'], df['DRtg'], c=df['Agg_Cluster'], cmap='plasma', label='Agg Clusters')
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.scatter(every_szn_df['ORtg'], every_szn_df['DRtg'], c='black', alpha=0)  # Use a constant color and make the points transparent
+
+    # Visualize the clusters
+    # plt.scatter(df['ORtg'], df['DRtg'], c=df['KMeans_Cluster'], cmap='viridis', label='KMeans Clusters')
+    plt.scatter(every_szn_df['ORtg'], every_szn_df['DRtg'], c=every_szn_df['Agg_Cluster'], cmap='plasma', label='Agg Clusters')
         
-    #     # Annotate points with team names
-    #     for i, team_name in enumerate(df['Team']):
-    #         plt.annotate(team_name, (df['ORtg'].iloc[i], df['DRtg'].iloc[i]), textcoords="offset points", xytext=(0, 5), ha='center', fontsize=6)
+    # Annotate points with team names
+    for i, team_name in enumerate(every_szn_df['Team']):
+        plt.annotate(team_name, (every_szn_df['ORtg'].iloc[i], every_szn_df['DRtg'].iloc[i]), textcoords="offset points", xytext=(0, 5), ha='center', fontsize=6)
         
-    #     # Make the quadrants and the diagonal
-    #     ax.plot([.5, .5], [0, 1], transform=ax.transAxes, alpha=0.15)
-    #     ax.plot([0, 1], [.5, .5], transform=ax.transAxes, alpha=0.15)
-    #     ax.plot([.1, .9], [.9, .1], transform=ax.transAxes, alpha=0.15)
+    # Make the quadrants and the diagonal
+    ax.plot([.5, .5], [0, 1], transform=ax.transAxes, alpha=0.15)
+    ax.plot([0, 1], [.5, .5], transform=ax.transAxes, alpha=0.15)
+    ax.plot([.1, .9], [.9, .1], transform=ax.transAxes, alpha=0.15)
 
 
-    #     plt.xlabel('ORtg')
-    #     plt.ylabel('DRtg')
-    #     plt.title(f'Clusters for {key}')
-    #     plt.legend()
-    #     plt.show()
+    plt.xlabel('ORtg')
+    plt.ylabel('DRtg')
+    plt.title(f'Clustering like teams since 1985')
+    plt.legend()
+    plt.show()
 
 
 
